a5b68e316d81da2a32880e3a6465cf4a11f7d2b2
anton <raudin@mail.com>
2025-04-10 21:28:35 +1000

    add: test_func

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,4 +1,4 @@
-from main.app import summ, delete
+from main.app import summ, delete, func
 
 def test_summ():
     result = summ(2, 5)
@@ -8,3 +8,8 @@ def test_summ():
 def test_delete():
     result = delete(6, 2)
     assert 3 != result
+
+
+def test_func():
+    result = func(2, 1)
+    assert 1 == result
