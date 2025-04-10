c285a2462dfef5718a405cae5eadf21077532867
anton <raudin@mail.com>
2025-04-10 21:06:57 +1000

    correct: test_app.py

--- main/tests/test_app.py
+++ main/tests/test_app.py
@@ -1,5 +1,10 @@
-from main.app import summ
+from main.app import summ, delete
 
 def test_summ():
     result = summ(2, 5)
     assert 7 == result
+
+
+def test_delete():
+    result = delete(6, 2)
+    assert 3 != result
