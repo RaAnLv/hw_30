268488e7fc32215ea75fb5f658c4e5618b76188c
anton <raudin@mail.com>
2025-04-11 20:12:03 +1000

    correct: black util

--- main/database.py
+++ main/database.py
@@ -1,5 +1,4 @@
-from sqlalchemy.ext.asyncio import (AsyncEngine, AsyncSession,
-                                    create_async_engine)
+from sqlalchemy.ext.asyncio import AsyncEngine, create_async_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
