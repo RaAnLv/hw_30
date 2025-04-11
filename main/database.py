e984543fa44708c91d978324aabf69283849f1ad
anton <raudin@mail.com>
2025-04-11 20:10:22 +1000

    correct: isort util

--- main/database.py
+++ main/database.py
@@ -1,4 +1,5 @@
-from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, AsyncEngine
+from sqlalchemy.ext.asyncio import (AsyncEngine, AsyncSession,
+                                    create_async_engine)
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
