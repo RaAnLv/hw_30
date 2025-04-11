e7bd002e7136acb1e31bf4a6320823546f518556
anton <raudin@mail.com>
2025-04-11 19:45:54 +1000

    add: fastapi app

---
+++ main/main.py
@@ -0,0 +1,53 @@
+from typing import List
+
+from fastapi import FastAPI, Request
+from sqlalchemy.future import select
+
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+import models
+import schemas
+from database import engine, session
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
+
+@app.on_event("startup")
+async def shutdup():
+    async with engine.begin() as conn:
+        await conn.run_sync(models.Base.metadata.create_all)
+
+
+@app.on_event("shutdown")
+async def shutdown():
+    await session.close()
+    await engine.dispose()
+
+
+@app.post('/recipes/', response_model=schemas.BookOut)
+async def cook_books(resept: schemas.BookIn) -> models.CookingBook:
+    new_resept = models.CookingBook(**resept.dict())
+    async with session.begin():
+        session.add(new_resept)
+    return new_resept
+
+
+@app.get('/recipes/{recipe_id}', response_model=schemas.BookOut)
+async  def one_book(request: Request, recipe_id: int) -> models.CookingBook:
+    res = await session.execute(select(models.CookingBook).where(models.CookingBook.id == recipe_id))
+    result = res.scalars().one()
+    result.count += 1
+    await session.commit()
+    return templates.TemplateResponse(name="detailed_information.html", context={"request": request, 'cook_book':result})
+
+
+@app.get('/recipes/', response_model=List[schemas.BookOut])
+async def all_books(request: Request) -> List[models.CookingBook]:
+    res = await session.execute(select(models.CookingBook))
+    cook_books = res.scalars().all()
+    return templates.TemplateResponse(name="index.html", context={"request":request, 'cook_books':cook_books})
+    #return res.scalars().all()
---
+++ main/schemas.py
@@ -0,0 +1,18 @@
+from pydantic import BaseModel
+
+
+class BaseBook(BaseModel):
+    name: str
+    product_list: str
+    time_cook: str
+    description: str
+
+class BookIn(BaseBook):
+    ...
+
+class BookOut(BaseBook):
+    id: int
+    count: int
+
+    class Config:
+        orm_mode = True
